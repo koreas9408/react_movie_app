adbae4eb1a0d4da444488cdfc320e5c0e8f5badb
SeungHyun <koreas9408@gmail.com>
2019-12-26 18:02:50 +0900

    propTypes 체크

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // component는 재사용 가능한 HTML을 생성한다.
 // props == properties. 컴포넌트에 데이터를 전달한다.
 // JSX = JavaScript + XML ( HTML )
 // prop의 이름과 인지값의 이름은 같아야한다.
 
-function Movie({ name }) {
+function Movie({ name, rating }) {
   return (
     <div>
-      <h2>I like {name}</h2>
+      <h2>I like {name} </h2>
+      <h4>{rating} / 5</h4>
     </div>
   );
 }
 
 const movieLike = [
   {
-    name: "Hello"
+    id: 1,
+    name: "Hello",
+    rating: 5
   },
   {
-    name: "JSX"
+    id: 2,
+    name: "JSX",
+    rating: 3
   },
   {
-    name: "React"
+    id: 3,
+    name: "React",
+    rating: 2
   }
 ];
 
+Movie.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 function App() {
   return (
     <div>
       {movieLike.map(item => (
-        <Movie name={item.name} />
+        <Movie key={item.id} name={item.name} rating={item.rating} />
       ))}
       {/* <Movie fav="react" />
       <Movie fav="JSX" /> */}
