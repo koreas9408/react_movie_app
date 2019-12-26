824532b10a4d030abf0a05eecdfec94bf03e0caa
SeungHyun <koreas9408@gmail.com>
2019-12-26 20:54:26 +0900

    Updates

---
+++ precache-manifest.daecdc0d7ff3bcdd0426d35ed8026640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f3c130d651796375dce4d585a46439",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "22d862b56800a603d0d5",
+    "url": "/react_movie_app/static/css/main.17406247.chunk.css"
+  },
+  {
+    "revision": "8526568d949b0ffebe66",
+    "url": "/react_movie_app/static/js/2.a187f9e2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react_movie_app/static/js/2.a187f9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22d862b56800a603d0d5",
+    "url": "/react_movie_app/static/js/main.3f686879.chunk.js"
+  },
+  {
+    "revision": "56d24d09734879d72d75",
+    "url": "/react_movie_app/static/js/runtime-main.aaff6312.js"
+  }
+]);
